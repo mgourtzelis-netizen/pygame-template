f7e12c297401fff717870bf840405527f8b4ec6f
mgourtzelis-netizen <mgourtzelis@gmail.com>
2026-07-13 16:24:41 -0700

    Add files via upload

---
+++ game.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.init() #initialization of pygame
+screen = pygame.display.set_mode((800, 600))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0, 0, 0))  # Fill the screen with black
+    pygame.display.flip()  # Update the display
