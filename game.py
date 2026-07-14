9256e72d9cf555a12b8213c4faea325e92a3c794
Milo <mgourtzelis@gmail.com>
2026-07-13 20:59:46 -0700

    add comments to template game.py

--- game.py
+++ game.py
@@ -1,12 +1,12 @@
 import pygame
 pygame.init() #initialization of pygame
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((800, 600)) # creates a window of 800x600 pixels
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+running = True # variable to control the main loop
+while running: # runs if running is true
+    for event in pygame.event.get(): # constantly checking for events
+        if event.type == pygame.QUIT: # checks if the user has clicked the close button
+            running = False # changes running to false and breaks the running loop
 
     screen.fill((255, 0, 0))  # Fill the window with red
     pygame.display.flip()  # Update the display
